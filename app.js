da28abdd861f4d294e518c4cdd55ae3380ed3a91
Soumik Saha <sahasoumik1573@gmail.com>
2023-04-09 18:07:20 +0530

    challenge 12 done

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {homeData: homeStartingContent});
-  console.log(posts);
+  res.render("home", {homeData: homeStartingContent, postsData: posts});
 })
 
 app.get("/about", function(req, res) {
